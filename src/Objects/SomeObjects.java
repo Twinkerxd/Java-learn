df8f4b78d76ea83bebe48f48afe4d3ffe6af45d1
Serega <msv-oo@yandex.ru>
2022-05-23 23:14:44 +0200

    Some object practice

---
+++ src/Objects/SomeObjects.java
@@ -0,0 +1,22 @@
+package Objects;
+
+public class SomeObjects {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Pixi", 1);
+        animal.sayHello();
+    }
+}
+
+class Animal {
+    private String name;
+    private int age;
+
+    Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void sayHello() {
+        System.out.println("Hello! My name is "+name+" and my age is "+age);
+    }
+}
